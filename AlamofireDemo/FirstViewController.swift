4d8d720df53d01fcc9409aed55ce1f9a0453ff30
lizhi0123 <zhang_zhi_li@163.com>
2017-03-20 10:06:47 +0800

    项目提交

---
+++ AlamofireDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AlamofireDemo
+//
+//  Created by SunnyZhang on 17/3/20.
+//  Copyright © 2017年 Sunny. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
